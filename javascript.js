5e9b7ec20440a534ac16a4164367489c168f73a6
Vanesa <vaneebg@gmail.com>
2022-05-10 13:36:49 +0200

    hecho extra sin revisar

--- javascript.js
+++ javascript.js
@@ -43,3 +43,77 @@ class Profesor extends Persona {
 };
 let profesor1 = new Profesor("Marcos", 40, "masculino", "Historia", "Universidad")
 console.log(profesor1.asignar())
+
+// EXTRAS
+// Construye las siguientes clases:
+console.log("EXTRAS")
+    // Warrior:
+    // constructor(life, power): Establece el valor de las propiedades life y power
+    // attack: Devuelve el valor de power del guerrero
+    // defend(damage): resta el valor del parámetro recibido damage al valor de la propiedad life. Después, imprime el valor de la vida restante.
+class Warrior {
+    constructor(life, power) {
+        this.life = life;
+        this.power = power;
+
+    }
+    attack() {
+        return `Tu poder es ${this.power}`;
+    }
+    defend(damage) {
+        return `Tu vida restante es ${this.life-damage}`;
+    }
+}
+
+let warrior1 = new Warrior(160, 70);
+console.log(warrior1.attack())
+console.log(warrior1.defend(100));
+
+// Maya: extiende de la clase Warrior
+// constructor: Aquí establecemos los valores para el maya, que no hay que olvidar que es un guerrero.
+// drinkColaCao: Suma 10 al poder.
+class Maya extends Warrior {
+    constructor(life, power, magic) {
+        super(life, power)
+        this.magic = magic;
+
+    }
+    drinkColaCao() {
+        return `Tu poder maya es ${this.power+10}`;
+    }
+
+}
+
+let maya1 = new Maya(160, 70, "water");
+// console.log(maya1.drinkColaCao())
+
+
+// Aztec: extiende de la clase Warrior
+// constructor: Aquí establecemos los valores para el azteca, que no hay que olvidar que es un guerrero.
+// drinkNesquik: Suma 10 a la vida.
+class Aztec extends Warrior {
+    constructor(life, power, shell) {
+        super(life, power)
+        this.shell = shell;
+
+    }
+    drinkNesquik() {
+        return `Tu vida azteca es ${this.life+10}`;
+    }
+
+}
+
+let aztec1 = new Aztec(160, 70, "steel");
+// console.log(aztec1.drinkNesquik())
+
+// Realiza la siguiente cadena de intercambio de golpes.
+// Azteca bebe nesquik
+console.log(aztec1.drinkNesquik())
+    // Maya bebe Cola Cao
+console.log(maya1.drinkColaCao())
+    // Maya ataca a azteca. Azteca defiende.
+console.log("atacando", maya1.attack(aztec1))
+console.log("defendiendo", aztec1.defend(aztec1.life - maya1.power))
+    // Azteca ataca a maya. Maya defiende.
+console.log("atacando", aztec1.attack(maya1))
+console.log("defendiendo", maya1.defend(maya1.life - aztec1.power))
